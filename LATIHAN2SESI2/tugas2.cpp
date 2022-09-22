c126099c4a95b5b628660b83296ebf55745c22e9
reyy27 <113427200+reyy27@users.noreply.github.com>
2022-09-22 23:09:37 +0700

    Add files via upload

---
+++ LATIHAN2SESI2/tugas2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define harga 4500.02
+using namespace std;
+
+
+int main()
+{ float jumlah,total;
+  cout << "masukan jumlah barang=";
+  cin >>jumlah; 
+  total=harga*jumlah;
+  cout << "\nyang harus di bayar=" << total << endl;
+  
+  
+  return 0;
+  }
+
